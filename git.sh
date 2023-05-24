2d97466f6aeba06cb5843382e73290261a9c1f91
kdbiosdeveloper <a.o.ashurov@gmail.com>
2023-05-24 13:19:29 +0500

    Changes committed: Wed May 24 13:19:29 +05 2023 from KDBs-MacBook-Pro.local

--- git.sh
+++ git.sh
@@ -4,5 +4,5 @@ HOST=$(hostname)
 git switch dev
 git add .
 git commit -m "Changes committed: $TODAY from $HOST"
-
+git remote set-url origin git@github.com:aashurov/EDSystems.Front.git/
 git push -u origin dev
