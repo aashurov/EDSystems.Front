f1762ed279a8f018b85ee7266cdb60eb50871838
kdbiosdeveloper <a.o.ashurov@gmail.com>
2023-05-24 13:17:53 +0500

    Changes committed: Wed May 24 13:17:53 +05 2023 from KDBs-MacBook-Pro.local

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 
+let myUrl ="https://localhost:7271/api/";
+if( window.location.hostname === "localhost")
+{
+    // console.log(window.location.hostname)
+}
+else{
+    //console.log(window.location.hostname)
+    myUrl = "https://www.roagt.ml/api/"
+}
+
 export const request = axios.create({
-    //baseURL: "https://www.roagt.ml/api/",
-     baseURL: "https://localhost:7271/api/",
-    
+     
+    baseURL: myUrl,
     headers: {"Authorization" : `Bearer ${localStorage.getItem("token")}`}
 })
